8592241a8f61b4f2775db5b612c90d890d9660d4
Aman Kumar <aman.kumar@vegease.in>
2026-05-10 23:11:27 +0530

    fix gmail ip4 issue

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import * as dns from 'dns';
+dns.setDefaultResultOrder('ipv4first');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
